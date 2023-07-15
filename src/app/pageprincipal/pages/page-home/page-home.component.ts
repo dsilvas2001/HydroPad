175a815bbd31b8819c02281e5cdde2ea9c34d75c
Donald Silva <dsilvas@uteq.edu.ec>
2023-07-14 23:27:28 -0500

    Contactanos Sin Iconos

--- src/app/pageprincipal/pages/page-home/page-home.component.ts
+++ src/app/pageprincipal/pages/page-home/page-home.component.ts
@@ -1,11 +1,62 @@
-import { Component, Input  } from '@angular/core';
+import { Component, AfterViewInit, Renderer2,NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.css']
-})
-export class PageHomeComponent {
-  @Input() load: boolean = true;
+  styleUrls: ['./page-home.component.css']})
+
+
+
+export class PageHomeComponent implements AfterViewInit{
+
+  constructor(private renderer: Renderer2) {}
+
+
+
+
+
+
+
+
+
+
+
+
+  ngAfterViewInit() {
+    const script = this.renderer.createElement('script');
+    script.text = `
+    const menu = document.querySelector('.nav__menu');
+    const menuList = document.querySelector('.nav__list');
+    const links = document.querySelectorAll('.nav__link');
+    
+    const modal = document.querySelector('.modal');
+    const modalButtonClose = document.querySelector('.modal__close');
+    
+    menu.addEventListener('click', function () {
+        menuList.classList.toggle('nav__list--show');
+    });
+    
+    links.forEach(function (link) {
+        link.addEventListener('click', function () {
+            menuList.classList.remove('nav__list--show');
+        });
+    });
+    
+    // MODAL
+    
+    // Se ejecuta cuando se carga el DOM
+    document.addEventListener('DOMContentLoaded', () => {
+        modal.classList.add('modal--visible');
+        modalButtonClose.addEventListener('click', () => {
+            modal.classList.remove('modal--visible');
+        });
+    });
+    `;
+    this.renderer.appendChild(document.body, script);
+  }
+  
+ 
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// export const environment = {
+//   firebase: {
+//     projectId: 'healthtest-94078',
+//     appId: '1:750733097606:web:5eef4d133cab0f12122f07',
+//     storageBucket: 'healthtest-94078.appspot.com',
+//     apiKey: 'AIzaSyCb7f5H0sC3T4MMsNYe-Z6HNlJVtESTsBM',
+//     authDomain: 'healthtest-94078.firebaseapp.com',
+//     messagingSenderId: '750733097606',
+//   },
+//       production: false,
+//       firebaseConfig: {
+//         apiKey: "AIzaSyCb7f5H0sC3T4MMsNYe-Z6HNlJVtESTsBM",
+//         authDomain: "healthtest-94078.firebaseapp.com",
+//         projectId: "healthtest-94078",
+//         storageBucket: "healthtest-94078.appspot.com",
+//         messagingSenderId: "750733097606",
+//         appId: "1:750733097606:web:5eef4d133cab0f12122f07"
+//       }
+//   };
+
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:4200'
+//   };
+  
