d6c4626b19b9fd93a96ce127db15bc118a754939
Donald Silva <dsilvas@uteq.edu.ec>
2023-07-19 07:42:42 -0500

    Hallazgo

--- src/app/pageprincipal/pages/page-home/page-home.component.ts
+++ src/app/pageprincipal/pages/page-home/page-home.component.ts
@@ -5,7 +5,7 @@ import { Component, AfterViewInit, Renderer2,NgModule ,CUSTOM_ELEMENTS_SCHEMA} f
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.css','./CSS/Contact.css','./CSS/Icono.css','./CSS/CardCreadores.css','./CSS/Services.css','./CSS/header.css']})
+  styleUrls: ['./page-home.component.css','./CSS/Contact.css','./CSS/Icono.css','./CSS/CardCreadores.css','./CSS/Services.css','./CSS/header.css','./CSS/buttonDescarga.css', './CSS/hallazgoClave.css']})
 
 
 
@@ -13,50 +13,11 @@ export class PageHomeComponent implements AfterViewInit{
 
   constructor(private renderer: Renderer2) {}
 
-
-
-
-
-
-
-
-
-
-
-
   ngAfterViewInit() {
-    const script = this.renderer.createElement('script');
-    script.text = `
-    const menu = document.querySelector('.nav__menu');
-    const menuList = document.querySelector('.nav__list');
-    const links = document.querySelectorAll('.nav__link');
-    
-    const modal = document.querySelector('.modal');
-    const modalButtonClose = document.querySelector('.modal__close');
-    
-    menu.addEventListener('click', function () {
-        menuList.classList.toggle('nav__list--show');
+    const arribaSpan = this.renderer.selectRootElement('.arriba');
+    this.renderer.listen(arribaSpan, 'click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     });
-    
-    links.forEach(function (link) {
-        link.addEventListener('click', function () {
-            menuList.classList.remove('nav__list--show');
-        });
-    });
-    
-    // MODAL
-    
-    // Se ejecuta cuando se carga el DOM
-    document.addEventListener('DOMContentLoaded', () => {
-        modal.classList.add('modal--visible');
-        modalButtonClose.addEventListener('click', () => {
-            modal.classList.remove('modal--visible');
-        });
-    });
-    `;
-    this.renderer.appendChild(document.body, script);
   }
-  
- 
 
 }
