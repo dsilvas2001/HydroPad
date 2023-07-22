eb3f662755a28c85991a8a6d4db9878c84ee18d3
Donald Silva <dsilvas@uteq.edu.ec>
2023-07-22 14:04:07 -0500

    Pagina Principal

--- src/app/pageprincipal/pages/page-home/page-home.component.ts
+++ src/app/pageprincipal/pages/page-home/page-home.component.ts
@@ -5,7 +5,7 @@ import { Component, AfterViewInit, Renderer2,NgModule ,CUSTOM_ELEMENTS_SCHEMA} f
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.css','./CSS/Contact.css','./CSS/Icono.css','./CSS/CardCreadores.css','./CSS/Services.css','./CSS/header.css','./CSS/buttonDescarga.css', './CSS/hallazgoClave.css']})
+  styleUrls: ['./page-home.component.css','./CSS/Contact.css','./CSS/Icono.css','./CSS/CardCreadores.css','./CSS/Services.css','./CSS/header.css','./CSS/buttonDescarga.css', './CSS/hallazgoClave.css', './CSS/buttonIniciarSesion.css']})
 
 
 
@@ -18,6 +18,13 @@ export class PageHomeComponent implements AfterViewInit{
     this.renderer.listen(arribaSpan, 'click', () => {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
+  }
+
+  scrollToSectionDesplzamiento(tiposeccion: string) {
+    const section = document.querySelector(tiposeccion);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
 }
